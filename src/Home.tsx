8094f93d8428090711232a34ad3b1d28b1493e6f
Pattaraporn-sri <pattaraporn.sri@kkumail.com>
2025-05-13 22:20:19 +0700

    [edit]: edit import in Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,15 @@
 import { Link } from "react-router-dom";
+import githublogo from "../src/img/icons8-github-logo-48.png"
+import facebooklogo from "../src/img/icons8-facebook-logo-50.png"
+import ig from "../src/img/icons8-instagram-logo-60.png"
+import yy from "../src/img/picture-yy-Photoroom.png"
+import visaulstudio from "../src/img/icons8-visual-studio-50.png"
+import library from "../src/img/library.png"
+import java from "../src/img/icons8-javascript-50.png"
+import github from "../src/img/icons8-github-50 (2).png"
+import tailwind from "../src/img/icons8-tailwind-css-100.png"
+import figma from "../src/img/icons8-figma-50.png"
+import coding from "../src/img/icons8-coding-50.png"
 
 function Home() {
   return (
@@ -11,26 +22,26 @@ function Home() {
           <p className="w-[580px] mt-2 font-Prompt font-light"> สนใจการพัฒนาเว็บไซต์ที่ใช้งานง่ายและตอบโจทย์ผู้ใช้ มีประสบการณ์ในการใช้ React และใส่ใจในด้าน UI/UX ตั้งใจพัฒนาทักษะเพื่อเติบโตในสายงาน Front-End Developer</p>
           <div className="flex mt-5">
             <Link to="https://github.com/Pattaraporn-sri" >
-          <img src="src/assets/icons8-github-logo-48.png" className="h-8"/>
+          <img src={githublogo} className="h-8"/>
           </Link>
           <Link to="https://www.facebook.com/pattaraporn.sritirat.2024">
-          <img src="src/assets/icons8-facebook-logo-50.png" className="h-8"/>
+          <img src={facebooklogo} className="h-8"/>
           </Link>
           <Link to="https://www.instagram.com/_yypatt/">
-          <img src="src/assets/icons8-instagram-logo-60.png" className="h-9"/>
+          <img src={ig} className="h-9"/>
           </Link>
           </div>
           
         </div>
 
-        <img src="src/assets/picture-yy-Photoroom.png" className="h-[600px] -mt-20 relative -ml-72"/>
-        <img src="src/assets/icons8-visual-studio-50.png" className="h-8 rotate-12 absolute ml-[1000px] mb-64"/>      
-        <img src="src/assets/library.png" className="absolute h-10 -rotate-12 ml-60 mb-48"/>  
-        <img src="src/assets/icons8-javascript-50.png" className="h-9 rotate-12 ml-[1000px] mb-16 absolute"/>
-        <img src="src/assets/icons8-github-50 (2).png" className="h-9 absolute ml-72 mb-96"/>
-        <img src="src/assets/icons8-tailwind-css-100.png" className="h-12 rotate-12 mb-[550px] absolute ml-[460px]"/>
-        <img src="src/assets/icons8-figma-50.png" className="absolute h-8 rotate-12 mb-[450px] ml-[950px]"/>
-        <img src="src/assets/icons8-coding-50.png" className="h-8 absolute ml-[750px] mb-[570px]"/>
+        <img src={yy} className="h-[600px] -mt-20 relative -ml-72"/>
+        <img src={visaulstudio} className="h-8 rotate-12 absolute ml-[1000px] mb-64"/>      
+        <img src={library} className="absolute h-10 -rotate-12 ml-60 mb-48"/>  
+        <img src={java} className="h-9 rotate-12 ml-[1000px] mb-16 absolute"/>
+        <img src={github} className="h-9 absolute ml-72 mb-96"/>
+        <img src={tailwind} className="h-12 rotate-12 mb-[550px] absolute ml-[460px]"/>
+        <img src={figma} className="absolute h-8 rotate-12 mb-[450px] ml-[950px]"/>
+        <img src={coding} className="h-8 absolute ml-[750px] mb-[570px]"/>
       </div>
     </>
   );
